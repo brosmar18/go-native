ad4e4e76c43f90575c3689677e651b8cf845a6cb
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-04 23:50:04 -0500

    categories section added to cedar-rapids-ia.html. Cards added as categories of recommendations

--- js/cedar-rapids-ia.js
+++ js/cedar-rapids-ia.js
@@ -2,5 +2,7 @@
 console.log('file connected');
 
 import { formsModule } from '../js-modules/formsModule.js';
+import { cardCategoriesModule } from '../js-modules/cardCategoriesModule.js';
 
+cardCategoriesModule.createCategoryCards();
 formsModule;
