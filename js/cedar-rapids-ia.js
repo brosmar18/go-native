31ddd8d3f50887a72e9dd71183119f71c78b1cbf
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-04 20:35:31 -0500

    nav links updated on cedar-rapids-.ia.html file

--- js/cedar-rapids-ia.js
+++ js/cedar-rapids-ia.js
@@ -1,8 +1,6 @@
 `use strict`
 console.log('file connected');
 
-import { cardDataModule } from '../js-modules/cardDataModule.js';
 import { formsModule } from '../js-modules/formsModule.js';
 
-cardDataModule.createCards();
 formsModule;
